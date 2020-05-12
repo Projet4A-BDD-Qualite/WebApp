9806f546decfa9509a35161fe40fed0a7ad19fbb
Madinee <madinevasimons@gmail.com>
2020-05-12 17:35:57 +0200

    initial pringboot project

---
+++ ensimWebApp/src/main/java/com/webapp/ensim/EnsimApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.ensim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsimApplication.class, args);
+	}
+
+}
